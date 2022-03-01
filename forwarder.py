c43af564deda6e0ea338307db3db457d58083bdf
Ben Thomasson <bthomass@redhat.com>
2022-03-01 14:50:10 -0500

    Adds headers to meta

--- forwarder.py
+++ forwarder.py
@@ -10,8 +10,9 @@ QUEUE_NAME = os.environ['QUEUE_NAME']
 FORWARDER_NAME = os.environ['FORWARDER_NAME']
 
 
-@app.route('/<endpoint>', methods=['POST', 'PUT', 'DELETE', 'PATCH'])
+@app.route('/<path:endpoint>', methods=['POST', 'PUT', 'DELETE', 'PATCH'])
 def webhook(endpoint):
+    print(request.headers)
     print(json.dumps(request.json))
 
     servicebus_client = ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR, logging_enable=True)
@@ -21,6 +22,7 @@ def webhook(endpoint):
         with sender:
             message = ServiceBusMessage(json.dumps(dict(payload=request.json,
                                                         meta=dict(endpoint=endpoint,
+                                                                  headers=request.headers,
                                                                   forwarder=FORWARDER_NAME))))
             sender.send_messages(message)
 
